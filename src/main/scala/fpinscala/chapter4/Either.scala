a817cf97df91c895daa920e25acde7e982dd114e
osmarluz <osmar.luz@gmail.com>
2018-04-10 23:11:48 -0300

    Add solution and test cases to exercise 4.7

--- src/main/scala/fpinscala/chapter4/Either.scala
+++ src/main/scala/fpinscala/chapter4/Either.scala
@@ -17,6 +17,14 @@ sealed trait Either[+E, +A] {
   }
 
   def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = for { a <- this; b1 <- b } yield f(a, b1)
+
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = {
+    es.foldRight[Either[E, List[A]]](Right(Nil))((a, b) => a.map2(b)(_ :: _))
+  }
+
+  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] = {
+    as.foldRight[Either[E, List[B]]](Right(Nil))((a, b) => f(a).map2(b)(_ :: _))
+  }
 }
 
 case class Left[+E](value: E) extends Either[E, Nothing]
--- src/test/scala/fpinscala/chapter4/EitherTestSuite.scala
+++ src/test/scala/fpinscala/chapter4/EitherTestSuite.scala
@@ -45,4 +45,39 @@ class EitherTestSuite extends FunSuite {
   test("returns the output of the input function lifted to the Either type") {
     assert(Right(5).map2(Right(5))(_ + _) == Right(10))
   }
+
+  //Exercise 4.7
+
+  test("returns Right(Nil) when the input list is Nil") {
+    assert(Left(e).sequence(Nil) == Right(Nil))
+    assert(Right(1).sequence(Nil) == Right(Nil))
+  }
+
+  test("returns the first Left encountered on the input list") {
+    val ee = new Exception("Test2");
+    assert(Left(e).sequence(List(Right(1), Right(2), Right(5), Left(e))) == Left(e))
+    assert(Left(e).sequence(List(Left(ee), Right(2), Right(5), Left(e))) == Left(ee))
+    assert(Right(1).sequence(List(Right(1), Right(2), Right(5), Left(e))) == Left(e))
+    assert(Right(1).sequence(List(Left(ee), Right(2), Right(5), Left(e))) == Left(ee))
+  }
+
+  test("returns Right(List()) when the input list is a List of Right types") {
+    assert(Left(e).sequence(List(Right(1), Right(2), Right(5))) == Right(List(1, 2, 5)))
+    assert(Right(1).sequence(List(Right(1), Right(2), Right(5))) == Right(List(1, 2, 5)))
+  }
+
+  test("returns Right(Nil) when the input list is Nil for traverse") {
+    assert(Left(e).traverse(Nil)(a => Right(a)) == Right(Nil))
+    assert(Right(1).traverse(Nil)(a => Right(a)) == Right(Nil))
+  }
+
+  test("returns Left(e) when the input function maps to at least one Left element") {
+    assert(Left(e).traverse(List(1, 2, 3, 4, 5))(a => if (a % 2 == 0) Right(a) else Left(e)) == Left(e))
+    assert(Right(1).traverse(List(1, 2, 3, 4, 5))(a => if (a % 2 == 0) Right(a) else Left(e)) == Left(e))
+  }
+
+  test("returns Right(List()) when the input function maps to no Left element") {
+    assert(Left(e).traverse(List(2, 4, 6, 8, 10))(a => if (a % 2 == 0) Right(a) else Left(e)) == Right(List(2, 4, 6, 8, 10)))
+    assert(Right(1).traverse(List(2, 4, 6, 8, 10))(a => if (a % 2 == 0) Right(a) else Left(e)) == Right(List(2, 4, 6, 8, 10)))
+  }
 }
