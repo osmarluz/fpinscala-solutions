c8ac36f4d5254c0fd2d008a6d21a62c766733a67
osmarluz <osmar.luz@gmail.com>
2018-04-13 23:15:51 -0300

    Modify description of tests on OptionTestSuite

--- src/test/scala/fpinscala/chapter4/OptionTestSuite.scala
+++ src/test/scala/fpinscala/chapter4/OptionTestSuite.scala
@@ -9,7 +9,7 @@ class OptionTestSuite extends FunSuite {
     assert(None.map((a: Int) => a + 1) == None)
   }
 
-  test("returns the Option mapped according to the input function") {
+  test("returns Some mapped according to the input function") {
     assert(Some(5).map(_ + 1) == Some(6))
   }
 
@@ -17,7 +17,7 @@ class OptionTestSuite extends FunSuite {
     assert(None.flatMap((a: Int) => Some(a + 5)) == None)
   }
 
-  test("returns the Option flat mapped according to the input function") {
+  test("returns Some flat mapped according to the input function") {
     assert(Some(5).flatMap(a => Some(a + 5)) == Some(10))
   }
 
@@ -26,16 +26,16 @@ class OptionTestSuite extends FunSuite {
     assert(None.getOrElse(Some(5)) == Some(5))
   }
 
-  test("returns the Option value after applying getOrElse") {
+  test("returns the Some value after applying getOrElse") {
     assert(Some(5).getOrElse(None) == 5)
   }
 
-  test("returns the default value when orElse is applied to None") {
+  test("returns the default Option when orElse is applied to None") {
     assert(None.orElse(None) == None)
     assert(None.orElse(Some(5)) == Some(5))
   }
 
-  test("returns the input Option after applying orElse") {
+  test("returns Some after applying orElse to Some") {
     assert(Some(5).orElse(None) == Some(5))
     assert(Some(5).orElse(Some(5)) == Some(5))
   }
@@ -48,7 +48,7 @@ class OptionTestSuite extends FunSuite {
     assert(Some(5).filter(_ > 5) == None)
   }
 
-  test("returns the input Option when the input function on filter is satisfied") {
+  test("returns Some when the input function on filter is satisfied") {
     assert(Some(5).filter(_ <= 5) == Some(5))
   }
 
