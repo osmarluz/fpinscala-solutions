5e20850c89de3b0bf4dd28f6336604a0b5c55a38
osmarluz <osmar.luz@gmail.com>
2018-04-16 23:09:57 -0300

    Add solution to exercise 5.2

--- src/main/scala/fpinscala/chapter5/Stream.scala
+++ src/main/scala/fpinscala/chapter5/Stream.scala
@@ -1,9 +1,23 @@
 package fpinscala.chapter5
 
+import Stream._
+
 sealed trait Stream[+A] {
   def toList: List[A] = this match {
     case Cons(h, t) => h() :: t().toList
     case _ => List()
+  }
+
+  def take(n: Int): Stream[A] = this match {
+    case Empty => empty
+    case Cons(h, _) if n == 1 => cons(h(), empty)
+    case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
+  }
+
+  @annotation.tailrec
+  final def drop(n: Int): Stream[A] = this match {
+    case Empty => this
+    case Cons(_, t) if n > 0 => t().drop(n - 1)
   }
 }
 case object Empty extends Stream[Nothing]
