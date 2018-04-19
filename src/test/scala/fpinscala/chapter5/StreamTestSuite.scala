092c9bb8bab3f0f29a498d1b81e7d26c57574a0c
osmarluz <osmar.luz@gmail.com>
2018-04-18 22:12:48 -0300

    Add test cases to exercise 5.2

--- src/test/scala/fpinscala/chapter5/StreamTestSuite.scala
+++ src/test/scala/fpinscala/chapter5/StreamTestSuite.scala
@@ -15,4 +15,34 @@ class StreamTestSuite extends FunSuite {
   test("returns the stream converted to a list when toList is applied to a non-empty stream") {
     assert(Stream(1, 2, 3, 4, 5).toList == List(1, 2, 3, 4, 5))
   }
+
+  //Exercise 5.2
+
+  test("returns empty stream when take is applied to an empty stream") {
+    assert(Empty.take(1) == Empty)
+    assert(Stream().take(1) == Empty)
+  }
+
+  test("returns the first n elements of the original stream when take is applied to a non-empty stream") {
+    assert(Stream(1).take(1).toList == Stream(1).toList)
+    assert(Stream(1).take(10).toList == Stream(1).toList)
+    assert(Stream(1, 2, 3, 4, 5).take(3).toList == Stream(1, 2, 3).toList)
+    assert(Stream(1, 2, 3, 4, 5).take(10).toList == Stream(1, 2, 3, 4, 5).toList)
+  }
+
+  test("returns empty stream when drop is applied to an empty stream") {
+    assert(Empty.drop(1) == Empty)
+    assert(Stream().drop(1) == Empty)
+  }
+
+  test("returns empty stream when the number of elements to drop is equal or larger than the stream size") {
+    assert(Stream(5).drop(1) == Empty)
+    assert(Stream(5).drop(10) == Empty)
+    assert(Stream(1, 2, 3, 4, 5).drop(10) == Empty)
+  }
+
+  test("discards the first n elements of the original stream when the number of elements to drop is smaller than the stream size") {
+    assert(Stream(1, 2, 3, 4, 5).drop(1).toList == Stream(2, 3, 4, 5).toList)
+    assert(Stream(1, 2, 3, 4, 5).drop(3).toList == Stream(4, 5).toList)
+  }
 }
