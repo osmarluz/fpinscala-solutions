6474789a8006c78ba186331c888fdda250460809
osmarluz <osmar.luz@gmail.com>
2018-04-27 23:19:53 -0300

    Add solution to exercise 5.4

--- src/main/scala/fpinscala/chapter5/Stream.scala
+++ src/main/scala/fpinscala/chapter5/Stream.scala
@@ -54,6 +54,8 @@ sealed trait Stream[+A] {
   }
 
   def exists(p: A => Boolean): Boolean = foldRight(false)((a, b) => p(a) || b)
+
+  def forAll(p: A => Boolean): Boolean = foldRight(true)((a, b) => p(a) && b)
 }
 
 case object Empty extends Stream[Nothing]
