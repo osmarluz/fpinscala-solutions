2008aecefca302cd02e92a7e72e11cd5fe82ae24
osmarluz <osmar.luz@gmail.com>
2018-07-05 23:17:56 -0300

    Add solution to zipAll in terms of unfold (part of exercise 5.13)

--- src/main/scala/fpinscala/chapter5/Stream.scala
+++ src/main/scala/fpinscala/chapter5/Stream.scala
@@ -89,6 +89,13 @@ sealed trait Stream[+A] {
     case (Cons(h1, t1), Cons(h2, t2)) => Some(f(h1(), h2()), (t1(), t2()))
     case _ => None
   }
+
+  def zipAll[B](s2: Stream[B]): Stream[(Option[A], Option[B])] = unfold((this, s2)) {
+    case (Cons(h1, t1), Cons(h2, t2)) => Some((Some(h1()), Some(h2())), (t1(), t2()))
+    case (Cons(h1, t1), Empty) => Some((Some(h1()), None), (t1(), empty))
+    case (Empty, Cons(h2, t2)) => Some((None, Some(h2())), (empty, t2()))
+    case (Empty, Empty) => None
+  }
 }
 
 case object Empty extends Stream[Nothing]
